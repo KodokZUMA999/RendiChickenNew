287b8b81673eb8a1c4f01a0b42cc24afe32e972f
KodokZUMA999 <owenfileprivate@gmail.com>
2024-08-06 16:23:12 +0700

    EWIBGIWEGU

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  base: '/RendiChickenNew/',  // Replace 'your-repo-name' with the name of your GitHub repository
 })
